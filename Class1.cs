5a9b4aee009c97574f0bdbdcc0b9d2684e34422e
Praful <prafulrakhade02@gmail.com>
2022-03-11 14:53:22 +0530

    [praful] Added: LeapYear Program File

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYearAndFlipCoin
+{
+    internal class Class1
+    {
+        public void LeapYear()
+        {
+            int year;
+            Console.Write(" Enter Year : ");
+            year = Convert.ToInt32(Console.ReadLine());
+            if (year % 400 == 0 || year % 100 != 0 && year % 4 == 0)
+            {
+                Console.WriteLine(year + " Is a Leap Year");
+            }
+            else
+            {
+                Console.WriteLine(year + " is not a Leap Year");
+            }
+        }
+    }
+}
