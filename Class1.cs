257986bdfe54d9070f6a472017c519faa6febf0b
Praful <prafulrakhade02@gmail.com>
2022-03-11 15:04:07 +0530

    [praful] Added: FlipCoin program File

--- Class1.cs
+++ Class1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LeapYearAndFlipCoin
 {
-    internal class Class1
+    public class Class1
     {
         public void LeapYear()
         {
@@ -21,6 +21,30 @@ namespace LeapYearAndFlipCoin
             {
                 Console.WriteLine(year + " is not a Leap Year");
             }
+        }
+        public void FlipCoin()
+        {
+            int heads = 0;
+            int tails = 0;
+            int coin;
+            Console.WriteLine("Enter the number : ");
+            coin = Convert.ToInt32(Console.ReadLine());
+            while (coin > 0)
+            {
+                Random random = new Random();
+                int randomCheck = random.Next(2);
+                if (randomCheck == 0)
+                {
+                    heads++;
+                }
+                else
+                {
+                    tails++;
+                }
+                coin--;
+            }
+            Console.WriteLine("Head = " + heads);
+            Console.WriteLine("Tails= " + tails);
         }
     }
 }
