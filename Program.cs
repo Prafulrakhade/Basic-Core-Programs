8ef5168bb6ca6cf6eafbeca5e6ddc09a732b0268
Praful <prafulrakhade02@gmail.com>
2022-03-12 23:46:28 +0530

    [praful] Added: Quotient And Remainder Program File

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace QuotientAndRemiander
+{
+    internal class Program
+    {
+        
+        public void QuotientAndRemiander()
+        {
+            int num1;
+            int num2;
+            int quotient;
+            int remainder;
+            Console.WriteLine("enter num1 =");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter num2 =");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            quotient = num1 / num2;
+            remainder = num1 % num2;
+            Console.WriteLine("Quotient = "+quotient);
+            Console.WriteLine("Remainder= "+remainder);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quotient And Remainder");
+            Program check = new Program();
+            check.QuotientAndRemiander();
+        }
+    }
+}
