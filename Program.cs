66ef412b5b89d5f020d6951987096c581232693f
Praful <prafulrakhade02@gmail.com>
2022-03-12 22:50:07 +0530

    [pradul] Added: Power of Two(2) program File

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PowerOf2
+{
+    internal class Program
+    {
+         public void PowerOf2()
+        {
+            int result = 1;
+            int a=2;
+            int b;
+            //Console.WriteLine("enter a ");
+            //a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter b ");
+            b = Convert.ToInt32(Console.ReadLine());
+            for (int i=1;i<=b;i++)
+            {
+                result = result * a;
+            }
+            Console.WriteLine("power of 2  upto num 3 = " + result);
+        }
+        static void Main(string[] args)
+        {
+            Program check = new Program();
+            check.PowerOf2();
+            
+        }
+    }
+}
