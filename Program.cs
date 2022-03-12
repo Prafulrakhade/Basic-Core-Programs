6c42652611e996b31c9653bf5b3361c98346d0ba
Praful <prafulrakhade02@gmail.com>
2022-03-12 23:58:23 +0530

    [praful] Added: Odd and Even Number Program File

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OddEven
+{
+    internal class Program
+    {
+        int num;
+        public void OddEven()
+        {
+            Console.Write("enter the number :");
+             num = Convert.ToInt32(Console.ReadLine());
+            if(num%2==0)
+            {
+                Console.WriteLine("the number is even");
+            }
+            else
+            {
+                Console.WriteLine("the number is odd");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("check number is even or odd");
+
+            Program obj = new Program();
+            obj.OddEven();
+        }
+    }
+}
