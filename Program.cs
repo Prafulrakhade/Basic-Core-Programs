69c41577fbee2742947e864ca3b0d5bc715691a7
Praful <prafulrakhade02@gmail.com>
2022-03-12 23:03:05 +0530

    [praful] Added: Harmonic Number program File

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HarmonicNumber
+{
+    internal class Program
+    {
+        public void  Harmonic()
+        {
+            double result = 0;
+            int num;
+            Console.Write("Enter the number :");
+            num=Convert.ToInt32( Console.ReadLine());
+            for(double i=1;i<=num;i++)
+            {
+                result = result + 1/i;
+            }
+            Console.WriteLine( "Harmonic number of " +num +" is "+result);
+        }
+        static void Main(string[] args)
+        {
+            Program check = new Program();
+            check.Harmonic();
+
+        }
+    }
+}
