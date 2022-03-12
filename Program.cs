bf8cc1947e732aa17d14e5ce06048d63bbc9a2c9
Praful <prafulrakhade02@gmail.com>
2022-03-12 23:18:55 +0530

    [praful] Added: Prime Factorization program file

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PrimeFactorization
+{
+    internal class Program
+    {
+        public void PrimeFactor(int num)
+        {
+           
+            int i;
+            for(i=2;i>1;i++)
+            {
+                while(num%i==0)
+                {
+                    Console.WriteLine(i);
+                    num = num / i;
+                }
+            }
+            Console.WriteLine(num);
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime Factor ");
+            Program check = new Program();
+            check.PrimeFactor(5);
+        }
+    }
+}
